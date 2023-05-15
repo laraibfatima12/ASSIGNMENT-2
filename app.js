bacf2eec945b54a7edc4ca478c4ba551312456ac
laraibfatima12 <128951988+laraibfatima12@users.noreply.github.com>
2023-05-15 10:27:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+//================= question 1 ===================//
+
+//1. Declare a variable called username.
+
+
+//let username
+
+
+//================= question 2  ==================//
+
+//2. Declare a variable called myName & assign to it a string that represents your full name .
+
+// let name ="laraib fatima"
+
+
+//================ question 3  =================//
+
+//3. declare a variable called myName & assign to it a string that represent your full Name.
+
+
+// let name;
+//message = "hello world"
+//alert (message);
+
+
+//================ question 4  ==================//
+
+//4. write a script to save student's bio data in js variables and show the data in alert box.
+
+//let student-name ="laraib fatima";
+//let student-aage= ="19";
+//let student qualification ="cerifiend web developer";
+//alert (student name);
+//alert0(student-age  + years old);
+//alert (qualification)
+
+
+//==================== question 5 ================//
+
+//5.write a script to display to following alert using one js variable.
+
+//PIZZA
+//PIZZ
+//PIZ
+//PI
+//PI
+
+
+//letName ="PIZZA";
+//Function trim (name){ while(name.length>=1){
+
+//alert (name.silice(0,i))
+//  }
+// }
